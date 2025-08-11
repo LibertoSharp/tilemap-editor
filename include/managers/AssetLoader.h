33dfe5d6ebdf4f051d7557966d00c0b8e83b1961
Liberto <liberto.sharp@gmail.com>
2025-08-11 20:11:18 +0200

    new assets can be put inside subfolders, asset id is now 'subfolder/asset_name'

--- include/managers/AssetLoader.h
+++ include/managers/AssetLoader.h
@@ -6,32 +6,37 @@
 #include "SFML/System/Exception.hpp"
 #include <iostream>
 
+using namespace  std::filesystem;
+
 template <typename Resource>
 class AssetLoader {
 protected:
     std::map<std::string, Resource*> resources;
 
 public:
-    explicit AssetLoader(std::filesystem::path path);
+    explicit AssetLoader(path path);
     void LoadResources();
 
 private:
-    std::filesystem::path localPath;
+    path localPath;
 };
 
 template<typename Resource>
-AssetLoader<Resource>::AssetLoader(std::filesystem::path path) : localPath(std::move(path)) {
+AssetLoader<Resource>::AssetLoader(path path) : localPath(std::move(path)) {
 
 }
 
 template<typename Resource>
 void AssetLoader<Resource>::LoadResources() {
-    for (const auto& entry : std::filesystem::directory_iterator(localPath)) {
+    for (const auto& entry : recursive_directory_iterator(localPath)) {
         if (entry.is_regular_file()) {
             Resource* resource = nullptr;
             try{
                 Resource* resource = new Resource(entry.path().string());
-                resources.insert(std::make_pair( entry.path().stem().string(),resource));
+                path relativePath = relative(entry, localPath);
+                relativePath = relativePath.parent_path() / relativePath.stem();
+                resources.insert(std::make_pair( relativePath.string(),resource));
+                std::cout << relativePath.string() << std::endl;
             } catch (sf::Exception& e) {
                 if (resource) delete resource;
             }
